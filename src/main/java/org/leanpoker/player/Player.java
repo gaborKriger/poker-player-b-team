a9726993ce75d15f689816dd5456c4848be3de91
Miklós Balázs <bazsi2382@gmail.com>
2019-01-14 14:46:47 +0100

    try

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -15,22 +15,30 @@ public class Player {
     public static int betRequest(JsonElement request) {
 
         try {
+            System.err.println("1");
             JsonObject root = request.getAsJsonObject();
+            System.err.println("2");
             JsonArray players = root.getAsJsonArray("players");
+            System.err.println("3");
             JsonArray communityCards = root.getAsJsonArray("community_cards");
-
+            System.err.println("4");
             List<String> holeCardsrank = new ArrayList<>();
             int myStack;
 
             for (int i = 0; i < players.size() ; i++) {
+                System.err.println("5");
                 JsonObject player = players.get(i).getAsJsonObject();
+                System.err.println("6");
                 JsonArray myCards = player.get("hole_cards").getAsJsonArray();
-
+                System.err.println("7");
                 String name = player.get("name").getAsString();
 
                 for (int j = 0; j < myCards.size(); j++) {
+                    System.err.println("8");
                     JsonObject cards = myCards.get(j).getAsJsonObject();
                     String rank = cards.get("rank").getAsString();
+
+                    System.err.println("9");
                     holeCardsrank.add(rank);
                 }
 
@@ -46,10 +54,13 @@ public class Player {
             }
 
             if(holeCardsrank.size() > 0){
+                System.err.println("10");
                 return 5;
-            } else {
-                return 2;
             }
+            System.err.println("11");
+            return 2;
+
+
 
 
         } catch (Exception e) {
