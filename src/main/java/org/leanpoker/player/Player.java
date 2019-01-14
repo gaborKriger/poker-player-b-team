b7a13ab0ee9304c7b9f6a7a2b0a27a03662a241c
gaborKriger <regirck@gmail.com>
2019-01-14 10:40:45 +0100

    JSON

--- src/main/java/org/leanpoker/player/Player.java
+++ src/main/java/org/leanpoker/player/Player.java
@@ -1,7 +1,10 @@
 package org.leanpoker.player;
 
+import com.google.gson.JsonArray;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
 
+import java.util.List;
 import java.util.Map;
 
 public class Player {
@@ -9,6 +12,13 @@ public class Player {
     static final String VERSION = "Version 0.1";
 
     public static int betRequest(JsonElement request) {
+
+        JsonObject root = request.getAsJsonObject();
+        JsonArray players = root.getAsJsonArray("players");
+        for (int i = 0; i < players.size(); i++) {
+
+        }
+
         return 0;
     }
 
